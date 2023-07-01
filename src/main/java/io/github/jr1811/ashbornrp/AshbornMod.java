d98088aa2c921899c093a01c9aa4bb59c9500ecc
JR1811 <janreichelt77@gmail.com>
2023-07-01 04:02:51 +0200

    hello there...

---
+++ src/main/java/io/github/jr1811/ashbornrp/AshbornMod.java
@@ -0,0 +1,25 @@
+package io.github.jr1811.ashbornrp;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AshbornMod implements ModInitializer {
+    public static final String MODID = "ashbornrp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info(MODID + " has been initialized");
+		devLogger(MODID + " has been started in a development environment");
+	}
+
+	public static void devLogger(String input) {
+		if (!FabricLoader.getInstance().isDevelopmentEnvironment()) return;
+		LOGGER.info("Dev - [ " + input + " ]");
+	}
+}
