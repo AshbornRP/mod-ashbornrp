40c1b930658bfa949309c361b78f2d9b18ed4857
JR1811 <janreichelt77@gmail.com>
2025-08-12 14:12:11 +0200

    fixed blindfold item default creative inv color

--- src/main/java/io/github/jr1811/ashbornrp/item/accessory/custom/BlindfoldAccessoryItem.java
+++ src/main/java/io/github/jr1811/ashbornrp/item/accessory/custom/BlindfoldAccessoryItem.java
@@ -1,9 +1,16 @@
 package io.github.jr1811.ashbornrp.item.accessory.custom;
 
+import io.github.jr1811.ashbornrp.cca.components.AccessoriesComponent;
 import io.github.jr1811.ashbornrp.item.accessory.IAccessoryItem;
 import io.github.jr1811.ashbornrp.util.Accessory;
+import io.github.jr1811.ashbornrp.util.AccessoryColor;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
 public class BlindfoldAccessoryItem extends ArmorItem implements IAccessoryItem {
     public BlindfoldAccessoryItem(ArmorMaterial material, Type type, Settings settings) {
@@ -13,5 +20,24 @@ public class BlindfoldAccessoryItem extends ArmorItem implements IAccessoryItem 
     @Override
     public Accessory getAccessoryType() {
         return Accessory.BLINDFOLD;
+    }
+
+    @Override
+    public ItemStack getDefaultStack() {
+        ItemStack stack = super.getDefaultStack();
+        if (!AccessoryColor.hasColors(stack)) {
+            AccessoryColor color = AccessoryColor.fromColors(0xffffff);
+            color.toStack(stack);
+        }
+        return stack;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        AccessoriesComponent accessoriesComponent = AccessoriesComponent.fromEntity(user);
+        if (accessoriesComponent == null) return super.use(world, user, hand);
+        ItemStack stack = user.getStackInHand(hand);
+        toggle(accessoriesComponent, stack);
+        return TypedActionResult.success(stack);
     }
 }
