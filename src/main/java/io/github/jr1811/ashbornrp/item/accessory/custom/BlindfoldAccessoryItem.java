56d7954d635e7d8ae947fadfadc1f7e9d487cade
JR1811 <janreichelt77@gmail.com>
2026-02-28 02:57:21 +0100

    potentially fixed BlindfoldAccessoryItem use interaction bug

--- src/main/java/io/github/jr1811/ashbornrp/item/accessory/custom/BlindfoldAccessoryItem.java
+++ src/main/java/io/github/jr1811/ashbornrp/item/accessory/custom/BlindfoldAccessoryItem.java
@@ -1,9 +1,11 @@
 package io.github.jr1811.ashbornrp.item.accessory.custom;
 
 import io.github.jr1811.ashbornrp.accessory.data.Accessory;
+import io.github.jr1811.ashbornrp.accessory.data.AccessoryEntryColors;
 import io.github.jr1811.ashbornrp.accessory.data.AccessoryEntryData;
 import io.github.jr1811.ashbornrp.compat.cca.components.AccessoriesComponent;
 import io.github.jr1811.ashbornrp.item.accessory.IAccessoryItem;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
@@ -11,9 +13,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BlindfoldAccessoryItem extends ArmorItem implements IAccessoryItem {
     public BlindfoldAccessoryItem(ArmorMaterial material, Type type, Settings settings) {
@@ -33,7 +39,13 @@ public class BlindfoldAccessoryItem extends ArmorItem implements IAccessoryItem 
             return TypedActionResult.fail(stack);
         }
         if (world instanceof ServerWorld serverWorld) {
-            component.addAccessory(true, getAccessoryType(), AccessoryEntryData.fromStack(stack));
+            AccessoryEntryColors color = AccessoryEntryColors.fromStack(stack);
+            if (color == null) color = AccessoryEntryColors.PLACEHOLDER.copy();
+            component.addAccessory(
+                    true,
+                    getAccessoryType(),
+                    new AccessoryEntryData(stack.copy(), color, true)
+            );
             serverWorld.playSound(
                     null,
                     user.getX(), user.getY(), user.getZ(),
@@ -43,5 +55,11 @@ public class BlindfoldAccessoryItem extends ArmorItem implements IAccessoryItem 
             if (!user.isCreative()) stack.decrement(1);
         }
         return TypedActionResult.success(stack);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+        IAccessoryItem.appendToolTip(stack, tooltip);
     }
 }
